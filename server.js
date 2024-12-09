dd797544ad356f302a0bcc3764321cc582806afd
Patrick Schroen <ps@ufotechnologies.com>
2024-12-09 18:59:41 -0500

    Add additional server details

--- server.js
+++ server.js
@@ -4,13 +4,20 @@
  * Remix of https://glitch.com/edit/#!/hello-express
  */
 
-import express from 'express';
-import { promisify } from 'util';
-import child_process from 'child_process';
+import { promisify } from 'node:util';
+import child_process from 'node:child_process';
 const exec = promisify(child_process.exec);
 
 let osRelease;
+let processorName;
 let numProcessingUnits;
+let ipinfo;
+let memTotal;
+let memFree;
+let swapTotal;
+let swapFree;
+let storageTotal;
+let storageAvailable;
 let serverUptime;
 let normalizedLoadAverage;
 
@@ -22,15 +29,72 @@ try {
 }
 
 try {
+	processorName = (await exec('lscpu | sed -nr "/Model name/ s/.*:\\s*(.*)/\\1/p" | tr -d "\\n"')).stdout;
+} catch (err) {
+	console.warn(err.stderr);
+}
+
+try {
 	numProcessingUnits = (await exec('nproc --all')).stdout;
 	numProcessingUnits = Number(numProcessingUnits);
 } catch (err) {
 	console.warn(err.stderr);
 }
 
-const app = express();
+try {
+	ipinfo = (await exec('curl https://ipinfo.io/')).stdout;
+	ipinfo = JSON.parse(ipinfo);
+} catch (err) {
+	console.warn(err.stderr);
+}
+
+async function getDetails() {
+	try {
+		let free = (await exec('free -b | tail -2 | tr -s " " | cut -d " " -f 2,4')).stdout;
+		free = free.split('\n').map(stdout => stdout.split(' '));
+		memTotal = Number(free[0][0]);
+		memFree = Number(free[0][1]);
+		swapTotal = Number(free[1][0]);
+		swapFree = Number(free[1][1]);
+	} catch (err) {
+		console.warn(err.stderr);
+	}
+
+	try {
+		let storage = (await exec('df -B1 . | tail -1 | tr -s " " | cut -d " " -f 2,4')).stdout;
+		storage = storage.split(' ');
+		storageTotal = Number(storage[0]);
+		storageAvailable = Number(storage[1]);
+	} catch (err) {
+		console.warn(err.stderr);
+	}
+
+	const data = {
+		// packageVersion: `${process.env.npm_package_name}/${process.env.npm_package_version}`,
+		projectDomain: `${process.env.PROJECT_DOMAIN}.glitch.me`,
+		networkName: `${ipinfo.hostname} (${ipinfo.ip})`,
+		serverVersion: `Node/${process.versions.node} (${osRelease})`,
+		processorName,
+		numProcessingUnits,
+		memTotal,
+		memFree,
+		swapTotal,
+		swapFree,
+		storageTotal,
+		storageAvailable
+	};
+
+	return data;
+}
+
+let serverDetails = await getDetails();
+console.log(serverDetails);
 
 //
+
+import express from 'express';
+
+const app = express();
 
 app.get('/server-status', async (req, res) => {
 	const currentTime = Math.floor(Date.now() / 1000); // seconds
@@ -44,19 +108,18 @@ app.get('/server-status', async (req, res) => {
 
 	try {
 		normalizedLoadAverage = (await exec('cat /proc/loadavg')).stdout;
-		normalizedLoadAverage = Number(normalizedLoadAverage.split(' ')[0]) / numProcessingUnits;
+		normalizedLoadAverage = Number(normalizedLoadAverage.split(' ')[0]) / numProcessingUnits; // 0 to 1 range
 		normalizedLoadAverage = Math.round((normalizedLoadAverage + Number.EPSILON) * 100) / 100;
 	} catch (err) {
 		console.warn(err.stderr);
 	}
 
 	res.json({
-		serverVersion: `Node/${process.versions.node} (${osRelease})`,
+		...serverDetails,
 		currentTime,
 		restartTime: currentTime - serverUptime,
 		serverUptime,
-		normalizedLoadAverage,
-		numProcessingUnits
+		normalizedLoadAverage
 	});
 });
 
